13ce4e536ca0b09c4811e40dab19a9f040fee4f4
Lue <krluckylue@gmail.com>
2019-09-14 17:59:50 +0900

    Add routing test code

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 //Load Module "express"
-const express = require('express')
-//Create app Object 
-const app = express()
+const express = require("express");
+//Create app Object
+const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on http://localhost:${PORT}`);
+//Call back funtion for routing test
+function handleProfile(req, res) {
+  res.send("You are on Profile");
 }
+
+function handleHome(req, res) {
+  console.log(req);
+  res.send("Hello From Home");
+}
+
+function handleListening() {
+  console.log(`Listening on http://localhost:${PORT}`);
+}
+
+app.get("/", handleHome);
+
+//routing test
+app.get("/profile", handleProfile);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.listen(PORT, handleListening);
