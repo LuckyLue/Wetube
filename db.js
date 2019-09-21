888b5ebca2ce93c1f907751ab463be0f4704b494
LuckyLue <45824665+LuckyLue@users.noreply.github.com>
2019-09-21 20:46:11 +0900

    Add mixins for Home Controller

--- db.js
+++ db.js
@@ -1,49 +1,54 @@
-export const videos = [{
-        id: 324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
-        creator: {
-            id: 445,
-            name: "Nicolas",
-            email: "nico@las.com"
-        }
-    },
-    {
-        id: 12345,
-        title: "Video super",
-        description: "This is something I love",
-        views: 33,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
-        creator: {
-            id: 335,
-            name: "Nicolas",
-            email: "nico@las.com"
-        }
-    },
-    {
-        id: 44442,
-        title: "Video nice",
-        description: "This is something I love",
-        views: 33,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
-        creator: {
-            id: 11,
-            name: "Nicolas",
-            email: "nico@las.com"
-        }
-    },
-    {
-        id: 1515,
-        title: "Video Good",
-        description: "This is something I love",
-        views: 33,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
-        creator: {
-            id: 335,
-            name: "Nicolas",
-            email: "nico@las.com"
-        }
+export const videos = [
+  {
+    id: 324393,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 445,
+      name: "Nicolas",
+      email: "nico@las.com"
     }
-]
+  },
+  {
+    id: 12345,
+    title: "Video super",
+    description: "This is something I love",
+    views: 33,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 335,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 44442,
+    title: "Video nice",
+    description: "This is something I love",
+    views: 33,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 11,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id: 1515,
+    title: "Video Good",
+    description: "This is something I love",
+    views: 33,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 335,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  }
+];
